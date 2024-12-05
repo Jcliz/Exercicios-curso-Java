0994a9348486325302683ac2a4bbb35df820d763
João Pedro Cardoso de Liz <joaopedrocliz@gmail.com>
2024-12-05 09:25:38 -0300

    Prática na criação de um HashSet homogêneo

---
+++ Arrays e Collections/src/Collections/ConjuntoOrganizado.java
@@ -0,0 +1,20 @@
+package Collections;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class ConjuntoOrganizado {
+    public static void main(String[] args) {
+        //Set<String> lista = new HashSet<>();
+        Set<String> lista = new TreeSet<>(); //respeita a ordem de inserção
+        lista.add("Oi");
+        lista.add("Tchau");
+        lista.add("João");
+        lista.add("Julia");
+
+        for (String candidatos : lista) {
+            System.out.println("Candidatos");
+        }
+    }
+}
