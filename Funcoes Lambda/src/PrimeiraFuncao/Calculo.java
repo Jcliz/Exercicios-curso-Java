fc7b9fdb4e505612075fc54caae336dbc2a92207
João Pedro Cardoso de Liz <joaopedrocliz@gmail.com>
2024-12-11 09:10:40 -0300

    Utilização da interface BinaryOperator

--- Funcoes Lambda/src/PrimeiraFuncao/Calculo.java
+++ Funcoes Lambda/src/PrimeiraFuncao/Calculo.java
@@ -1,5 +1,6 @@
 package PrimeiraFuncao;
 
+@FunctionalInterface
 public interface Calculo {
     double executar(double a, double b);
 }
---
+++ Funcoes Lambda/src/PrimeiraFuncao/MainLambda2.java
@@ -0,0 +1,16 @@
+package PrimeiraFuncao;
+
+import java.util.function.BinaryOperator;
+
+public class MainLambda2 {
+    public static void main(String[] args) {
+
+        //interface de operações binárias do Java
+        //recebe dois parâmetros de um tipo e retorna o mesmo tipo
+        BinaryOperator<Double> calc = (x, y) -> x + y;
+        System.out.println(calc.apply(2.0, 3.0));
+
+        calc = (x, y) -> x * y;
+        System.out.println(calc.apply(2.0, 3.0));
+    }
+}
