ebbacfda3a7b9340c28f77f2216b98a9e8fd6f47
João Pedro Cardoso de Liz <joaopedrocliz@gmail.com>
2024-12-12 08:50:12 -0300

    Prática com o Consumer

---
+++ Funcoes Lambda/src/InterfacesFuncionais/Consumer/Main.java
@@ -0,0 +1,23 @@
+package InterfacesFuncionais.Consumer;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class Main {
+    public static void main(String[] args) {
+        Consumer<Produto> imprimir = p -> System.out.println(p.getNome());
+
+        Produto p = new Produto("Yunu Uchuhai", 32.00, 0.15);
+        Produto p2 = new Produto("Computador", 3201.64, 0.35);
+        Produto p3 = new Produto("Monitor", 702.21, 0.25);
+        Produto p4 = new Produto("Mouse", 92.53, 0.10);
+        imprimir.accept(p);
+
+        List<Produto> produtos = Arrays.asList(p, p2, p3, p4);
+
+        produtos.forEach(imprimir);
+        produtos.forEach(prod -> System.out.println(prod.getPreco()));
+        produtos.forEach(System.out::println);
+    }
+}
