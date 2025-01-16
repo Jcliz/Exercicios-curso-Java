5018372ce633f2a5ead0ecf2a078bcfa046297ec
João Pedro Cardoso de Liz <joaopedrocliz@gmail.com>
2025-01-16 11:40:50 -0300

    Utilização dos Generics em métodos

---
+++ Generics/src/ListaUtil.java
@@ -0,0 +1,12 @@
+import java.util.List;
+
+public class ListaUtil {
+    //? == qualquer tipo
+    public static Object getUltimo1(List<?> lista) {
+        return lista.getLast();
+    }
+
+    public static <T> T getUltimo2(List<T> lista) {
+        return lista.getLast();
+    }
+}
