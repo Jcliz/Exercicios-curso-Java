c74f3c4a5efa25b64fdbc36fff199cb7b53f70ae
João Pedro Cardoso de Liz <joaopedrocliz@gmail.com>
2025-02-05 11:51:56 -0300

    Correção da conexão com o banco

--- src/jdbc/TesteConexao.java
+++ src/jdbc/TesteConexao.java
@@ -6,9 +6,9 @@ import java.sql.SQLException;
 
 public class TesteConexao {
     public static void main(String[] args) throws SQLException {
-        final String url = "jdbc:mysql://localhost:3006";
+        final String url = "jdbc:mysql://localhost:3306";
         final String usuario = "root";
-        final String senha = "";
+        final String senha = "123456";
 
         Connection conexao = DriverManager
                 .getConnection(url, usuario, senha);
