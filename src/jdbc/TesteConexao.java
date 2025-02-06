5a80abed46a700172e8ad6cc6527aece330f0d73
João Pedro Cardoso de Liz <joaopedrocliz@gmail.com>
2025-02-06 09:36:52 -0300

    Parâmetros para uma conexão segura

--- src/jdbc/TesteConexao.java
+++ src/jdbc/TesteConexao.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class TesteConexao {
     public static void main(String[] args) throws SQLException {
-        final String url = "jdbc:mysql://localhost:3306";
+        final String url = "jdbc:mysql://localhost:3306?verifyServerCertificate=false&useSSL=true";
         final String usuario = "root";
         final String senha = "123456";
 
